91e7bfa25f28cff25bf91a99de39cd2f09379829
Marufg <marufb572@gmail.com>
2023-08-28 00:02:52 +0600

    added a new file

--- js/phone.js
+++ js/phone.js
@@ -32,8 +32,8 @@ const displayPhone = (phones, isShowAll) => {
                     <div class="card-body">
                       <h2 class="card-title">${phone.phone_name}</h2>
                       <p>If a dog chews shoes whose shoes does he choose?</p>
-                      <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Buy Now</button>
+                      <div class="card-actions justify-center">
+                        <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">show details</button>
                       </div>
                     </div>
     `;
@@ -42,11 +42,45 @@ const displayPhone = (phones, isShowAll) => {
     toggleLoadingSpinner(false)
 }
 
+const handleShowDetail = async (id) =>{
+    // console.log('sadi', id)
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    
+
+    const phone = data.data;
+    showPhoneDetails(phone)
+
+}
+
+const showPhoneDetails = (phone) => {
+
+    const phoneName = document.getElementById('show-details-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailsContainer = document.getElementById('show-detail-container');
+    showDetailsContainer.innerHTML = `
+    <img src="${phone.image}" alt="" />
+    <p><span class="font-bold">storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p><span class="font-bold">display size:</span>${phone?.mainFeatures?.displaySize}</p>
+    <p><span class="font-bold">chipSet:</span>${phone?.mainFeatures?.chipSet}</p>
+    <p><span class="font-bold">memory:</span>${phone?.mainFeatures?.memory}</p>
+    <p><span class="font-bold">slug:</span>${phone?.slug}</p>
+    <p><span class="font-bold">releaseDate:</span>${phone?.releaseDate}</p>
+    <p><span class="font-bold">brand:</span>${phone?.brand}</p>
+    <p><span class="font-bold">GPS:</span>${phone?.others?.GPS}</p>
+    `
+    console.log(phone);
+    show_details_modal.showModal()
+
+}
+
 const handleSearch = (isShowAll) => {
     toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
     loadPhone(searchText, isShowAll)
 
 }
